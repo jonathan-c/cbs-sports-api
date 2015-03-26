d63f51eb5762c25a4c50842e5cc9fd8cb5006409
Jon Castillo <jcastillo@ghostery.com>
2015-03-25 20:11:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DraftCodeExercise::Application.config.secret_token = 'e41588535151fe39578efa3fe7ee1f74a9b8c6895f2bf549a286ba9cc3f8a6776b3c44d45c1676a9b9403814abd580343b9d170794cc32c83b543fd24c626ddf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DraftCodeExercise::Application.config.session_store :cookie_store, key: '_draft_code_exercise_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DraftCodeExercise::Application.config.session_store :active_record_store
