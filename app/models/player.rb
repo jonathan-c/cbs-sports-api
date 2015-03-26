8012d97e4b8f12fdc3575cd430607ee4f465c4a2
Jon Castillo <jcastillo@ghostery.com>
2015-03-25 23:09:08 -0400

    added name_brief method to Player class

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,19 @@
 class Player < ActiveRecord::Base
   attr_accessible :age, :first_name, :last_name, :position, :cbs_id, :sport
 
+  def name_brief
+    if self.sport == "basketball"
+      first_name+" "+last_name[0]+"."
+    elsif self.sport == "baseball"
+      fisrt_name[0]+". "+last_name[0]+"."
+    elsif self.sport == "football"
+      first_name[0]+". "+last_name
+    else
+    end
+  end
+
+  def average_position_age_diff
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,22 @@ DraftCodeExercise::Application.routes.draw do
 
   get "update_players/football"
 
+  # namespace :api do
+  #   namespace :baseball do
+  #     resources :players
+  #   end
+  #   namespace :basketball do
+  #     resources :players
+  #   end
+  #   namespace :football do
+  #     resources :players
+  #   end
+  # end
+
+  scope "/api" do
+    resources :basketball_players do
+    end
+  end
+
+
 end
