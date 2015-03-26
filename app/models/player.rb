d500a4ed8f4ad66c4a3e293953d69f6f1230453f
Jon Castillo <jcastillo@ghostery.com>
2015-03-26 00:37:45 -0400

    fixed issue with accurately calculating average_position_age_diff

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,20 +2,27 @@ class Player < ActiveRecord::Base
   attr_accessible :age, :first_name, :last_name, :position, :cbs_id, :sport
 
   def name_brief
-    if self.sport == "basketball"
-      first_name+" "+last_name[0]+"."
-    elsif self.sport == "baseball"
-      first_name[0]+". "+last_name[0]+"." if !first_name.blank?
-    elsif self.sport == "football"
-      first_name[0]+". "+last_name if !first_name.blank?
-    else
+    if !first_name.blank? && !last_name.blank?
+      case self.sport
+      when "basketball"
+        first_name+" "+last_name[0]+"."
+      when "baseball"
+        first_name[0]+". "+last_name[0]+"."
+      when "football"
+        first_name[0]+". "+last_name
+      else
+        ""
+      end
     end
   end
 
   def average_position_age_diff
-    # What if age is NULL?
-    # NULL values are causing this function to be inaccurate
-    Player.average(:age, :conditions => {:position => self.position, :sport => self.sport}).to_f
+    # There are errors in the CBS API. Some players are returning age 'NULL' or '2015'
+    # which resulted in inaccurate average_position_age_diff values
+    # Issue fixed by only querying players who are ages 1-100
+    players = Player.where('age > 0').where('age < 100').where(sport: self.sport, position: self.position)
+    ages = players.map{|player| player.age}
+    (ages.sum.to_f / ages.length).to_s
   end
 
 end
